370b41d5718ce3185e28b8b2f588cae28360521f
ivanmartovytskyi <132284814+ivanmartovytskyi@users.noreply.github.com>
2023-06-28 14:32:10 +0300

    add terraform files

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "GOOGLE_REGION" {
+  description = "Google Cloud Platform region for GKE cluster deployment"
+}
+
+variable "GOOGLE_PROJECT" {
+  description = "Google Cloud Platform project ID"
+}
+
+variable "GKE_NUM_NODES" {
+  description = "Number of nodes in the GKE cluster"
+}
---
+++ terraform/vars.tfvars
@@ -0,0 +1,3 @@
+GOOGLE_REGION = "us-central1"
+GOOGLE_PROJECT = "devops-038921093"
+GKE_NUM_NODES = 2
